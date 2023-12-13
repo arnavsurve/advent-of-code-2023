92fde4624957139788448e2208a5f26bf098a311
Arnav <arnavsurve@gmail.com>
2023-12-13 04:28:09 -0500

    day 1 part 2

--- day1/p2/main.py
+++ day1/p2/main.py
@@ -1,36 +1,21 @@
 import re
 
-file = open('example.txt', 'r')
+file = open('input.txt', 'r')
 lines = file.readlines()
 
+
+def get_digit(d):
+    if d.isnumeric():
+        return d
+    else:
+        return digits.index(d)
+
+digits = ('', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
+regex = r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))'
+
 count = 0
-
-def replace_number_word(match):
-    number_words = {
-        "zero": "0",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9"
-    }
-    return number_words[match.group(0)]
-
-
-
 for line in lines:
-    line = re.sub(r'\b(?:zero|one|two|three|four|five|six|seven|eight|nine)\b', replace_number_word, line)
-    print(line)
-    nums = []
-    for char in line:
-        if char.isdigit():
-            nums.append(int(char))
-    if len(nums) != 0:
-        value = int(str(nums[0]) + str(nums[-1]))
-        count += value
+    nums = re.findall(regex, line)
+    count += int(str(get_digit(nums[0])) + str(get_digit(nums[-1])))
 
 print(count)
